e37ef1cde23c30d69ab1aca38c2c87e4e93fb054
apatheticdev-saad <shaikhsaadofficial786@gmail.com>
2026-04-02 22:32:43 +0530

    sixth topic

---
+++ 06_useRef concept/src/App.jsx
@@ -0,0 +1,33 @@
+import { useRef } from "react"
+
+function App() {
+    const boxRef = useRef(null)
+
+    function makeRed() {
+        boxRef.current.style.backgroundColor = "red"
+    }
+
+    function makeGreen() {
+        boxRef.current.style.backgroundColor = "green"
+    }
+
+    function makeBlue() {
+        boxRef.current.style.backgroundColor = "blue"
+    }
+
+    return (
+        <>
+            <div
+                ref={boxRef}
+                style={{ width: "200px", height: "200px", backgroundColor: "gray" }}
+            >
+            </div>
+            <br />
+            <button onClick={makeRed}>Red</button>
+            <button onClick={makeGreen}>Green</button>
+            <button onClick={makeBlue}>Blue</button>
+        </>
+    )
+}
+
+export default App
